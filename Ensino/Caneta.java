04be09eb58d967adf5f7ea38082e80a55d7c87c3
HenriqueFrancaa <henrique23.franca@gmail.com>
2024-04-04 19:44:28 -0300

    AI MDS

---
+++ Ensino/Caneta.java
@@ -0,0 +1,12 @@
+
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void status(){
+        System.out.println("Uma caneta " + this.cor);
+    }
+}
